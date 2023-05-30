8ad84ceeced31d86ce38d0b6e2659197b827d955
theanuragshukla <tachyonanurag@gmail.com>
2023-05-30 15:04:11 +0530

    ⚡ init

---
+++ src/main/java/com/anurag/upi/UpiApplication.java
@@ -0,0 +1,13 @@
+package com.anurag.upi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpiApplication.class, args);
+	}
+
+}
